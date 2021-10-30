cb384f6390cbd64a14e2c537943c856e0c5b74e7
forsenforsenforsenforsen <90532281+forsenforsenforsenforsen@users.noreply.github.com>
2021-10-30 19:12:06 -0400

    Fixed description reference

--- BionicTraveler/Assets/BionicTraveler/Scripts/ButtonListQuest.cs
+++ BionicTraveler/Assets/BionicTraveler/Scripts/ButtonListQuest.cs
@@ -13,15 +13,26 @@ namespace BionicTraveler.Scripts
 
         [SerializeField]
         public Text quest;
+
+        public string n;
+        public string d;
         public Text questName;
         public Text questDescrip;
         public GameObject DescripBox;
-        private List<string> questNames = new List<string>();
-        private List<string> questDescrips = new List<string>();
+        //private static List<string> questNames = new List<string>();
+        //private static List<string> questDescrips = new List<string>();
+        private int i;
 
         public void setText(string name)
         {
+            n = name;
             quest.text = name;
+        }
+
+        public void setDesc(string Desc)
+        {
+            d = Desc;
+            questDescrip.text = Desc;
         }
 
         public void setBox(string name, string des)
@@ -29,13 +40,13 @@ namespace BionicTraveler.Scripts
             questName.text = name;
             questDescrip.text = des;
         }
-
-        public void addBox(string name, string des)
+/*
+        public static void addBox(string name, string des)
         {
-            questNames.Add(name);
-            questDescrips.Add(des);
+            //questNames.Add(name);
+            //questDescrips.Add(des);
         }
-
+*/
         public void onClick()
         {
             if (DescripBox.activeInHierarchy == true)
@@ -43,7 +54,9 @@ namespace BionicTraveler.Scripts
                 DescripBox.SetActive(false);
             } else
             {
+                
                 DescripBox.SetActive(true);
+                setBox(n, d);
             }
         }
     }
