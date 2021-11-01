b104ac940df59fcaacbc66e6ee62137f309cb33d
Nick Liu <86247452+liunicholas6@users.noreply.github.com>
2021-11-01 00:25:45 -0400

    Player and Dynamic Entities

---
+++ BionicTraveler/Assets/ScriptTemplates/DynamicEntity.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DynamicEntity : MonoBehaviour
+{
+    protected float speed;
+
+    protected Vector3 _direction;
+    protected bool _stunned;
+    public Vector3 Direction => _direction;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void StunLock()
+    {
+        
+    }
+}
