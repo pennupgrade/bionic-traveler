93b9139e9fc03a273d7afcbf785f35e3ca27c46f
Lennart <lennart@lms-dev.com>
2021-11-06 19:41:49 -0400

    More refactor

---
+++ BionicTraveler/Assets/BionicTraveler/Scripts/World/DynamicEntity.cs
@@ -0,0 +1,114 @@
+﻿namespace BionicTraveler.Scripts.World
+{
+    using System.Collections;
+    using BionicTraveler.Scripts.Items;
+    using UnityEngine;
+
+    /// <summary>
+    /// Class for all entities that can move themselves.
+    /// </summary>
+    public class DynamicEntity : Entity
+    {
+        private Vector3 direction;
+        private Vector3 velocity;
+        private bool stunned;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="DynamicEntity"/> class.
+        /// </summary>
+        public DynamicEntity()
+        {
+            this.Inventory = new Inventory(this);
+        }
+
+        /// <summary>
+        /// Gets the inventory.
+        /// </summary>
+        public Inventory Inventory { get; }
+
+        /// <summary>
+        /// Gets or sets direction for SpriteRenderer/FSM.
+        /// </summary>
+        internal Vector3 Direction { get => this.direction; set => this.direction = value; }
+
+        /// <summary>
+        /// Gets or sets velocity.
+        /// </summary>
+        internal Vector3 Velocity { get => this.velocity; set => this.velocity = value; }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether entity is stunned.
+        /// </summary>
+        internal bool IsStunned { get => this.stunned; set => this.stunned = value; }
+
+        /// <summary>
+        /// Function for moving a dynamic entity to a target position.
+        /// </summary>
+        /// <param name="target">Target world position to move to.</param>
+        public void MoveTo(Vector3 target, bool smooth = false)
+        {
+            this.SetDirection(target);
+
+            base.MoveTo(target, smooth);
+
+        }
+
+        /// <summary>
+        /// Sets Direction for this Dynamic Entity.
+        /// </summary>
+        /// <param name="target">Target world position to look at</param>
+        public void SetDirection(Vector3 target)
+        {
+            Vector3 pos = this.gameObject.transform.position;
+            float angle = Mathf.Rad2Deg * Mathf.Atan2(target.y - pos.y, target.x - pos.x);
+            if (angle > 315 || angle < 45)
+            {
+                this.Direction = Vector3.right;
+            }
+            else if (angle < 135)
+            {
+                this.Direction = Vector3.up;
+            }
+            else if (angle < 225)
+            {
+                this.Direction = Vector3.left;
+            }
+            else
+            {
+                this.Direction = Vector3.down;
+            }
+        }
+
+        /// <summary>
+        /// Makes DEntity invincible for specified number of milliseconds
+        /// </summary>
+        /// <param name="ms">Number of milliseconds to remain invincible</param>
+        public void IFrame(int ms)
+        {
+            this.StartCoroutine(this.IFrameHandler(ms));
+        }
+
+        /// <summary>
+        /// Stagger/Stun the entity for the specified number of milliseconds
+        /// </summary>
+        /// <param name="ms">The number of milliseconds to stun the entity</param>
+        public void Stagger(int ms)
+        {
+            this.StartCoroutine(this.StaggerHandler(ms));
+        }
+
+        private IEnumerator IFrameHandler(int ms)
+        {
+            this.IsInvincible = true;
+            yield return new WaitForSeconds(ms / 1000f);
+            this.IsInvincible = false;
+        }
+
+        private IEnumerator StaggerHandler(int ms)
+        {
+            this.IsStunned = true;
+            yield return new WaitForSeconds(ms / 1000f);
+            this.IsStunned = false;
+        }
+    }
+}
---
+++ BionicTraveler/Assets/BionicTraveler/Scripts/World/Entity.cs
@@ -0,0 +1,137 @@
+﻿namespace BionicTraveler.Scripts.World
+{
+    using System;
+    using System.Collections;
+    using BionicTraveler.Scripts.Combat;
+    using UnityEngine;
+
+    /// <summary>
+    /// Describes a base entity in the game world.
+    /// </summary>
+    public abstract class Entity : MonoBehaviour
+    {
+        private int maxHealth = 100;
+        private int health;
+        private bool visible = false;
+        [SerializeField]
+        private float baseSpeed = 1f;
+
+        /// <summary>
+        /// Gets or sets a value indicating whether entity is invincible.
+        /// </summary>
+        public bool IsInvincible { get; set; }
+
+        /// <summary>
+        /// Gets the health from the entity.
+        /// </summary>
+        public int Health => this.health;
+
+        /// <summary>
+        /// Add some fixed value to the current health value.
+        /// </summary>
+        /// <param name = "amt">The amount of health to be added</param>
+        public void AddHealth(float amt)
+        {
+            this.health = (int)Math.Min(this.health + amt, this.maxHealth);
+        }
+
+        /// <summary>
+        /// Decrease the current Health value by some fixed amount
+        /// </summary>
+        /// <param name="amt">The amount of health lost</param>
+        public void LoseHealth(float amt)
+        {
+            this.health = (int)Math.Max(this.health - amt, 0f);
+            Debug.Log($"{this.gameObject.name}: My health is now {this.health}");
+        }
+
+        /// <summary>
+        /// OnHit functionality to be implemented in children of Entity.
+        /// </summary>
+        /// <param name="attack">The attack.</param>
+        public virtual void OnHit(Attack attack)
+        {
+            if (this.IsInvincible)
+            {
+                return;
+            }
+
+            Debug.Log($"{this.gameObject.name} just got hit");
+            var damage = attack.AttackData.GetBaseDamage();
+            this.LoseHealth(damage);
+        }
+
+        /// <summary>
+        /// Move the entity to some specified destination
+        /// </summary>
+        /// <param name="dest">The Transform of the destination to move to</param>
+        /// <param name="smooth">Whether to use a SmoothStep function for the movement; default false</param>
+        public void MoveTo(Vector3 dest, bool smooth = false)
+        {
+            StartCoroutine(Movement(dest, smooth));
+        }
+
+        private IEnumerator Movement(Vector3 target, bool smooth)
+        {
+            Transform pos = gameObject.transform;
+            float duration = (pos.position - target).magnitude / baseSpeed;
+            float elapsed = 0f;
+            float t = elapsed / duration;
+            if (smooth) { t = t * t * (3f - 2f * t); }
+
+            while (elapsed < duration)
+            {
+                pos.position = Vector3.Lerp(pos.position, target, t);
+                elapsed += Time.deltaTime;
+                yield return null;
+            }
+
+            pos.position = target;
+
+        }
+
+        /// <summary>
+        /// Destroy the EntityGameObject
+        /// </summary>
+        public void Destroy()
+        {
+            Destroy(gameObject);
+        }
+
+        /// <summary>
+        /// Hides the entity in the current Scene
+        /// </summary>
+        public void MakeInvisible()
+        {
+            UnityEditor.SceneVisibilityManager.instance.Hide(gameObject, true);
+        }
+
+        /// <summary>
+        /// Shows/Un-hides the entity in the current Scene
+        /// </summary>
+        public void MakeVisible()
+        {
+            UnityEditor.SceneVisibilityManager.instance.Show(gameObject, true);
+        }
+
+        /// <summary>
+        /// Toggles the visibility state of the entity GameObject in the current Scene
+        /// </summary>
+        public void ToggleVisibility()
+        {
+            UnityEditor.SceneVisibilityManager.instance.ToggleVisibility(gameObject, true);
+        }
+
+        public bool IsVisible()
+        {
+            return visible;
+        }
+
+        // Use this for initialization
+        void Start()
+        {
+            health = maxHealth;
+            visible = !UnityEditor.SceneVisibilityManager.instance.IsHidden(gameObject, true);
+        }
+    }
+}
