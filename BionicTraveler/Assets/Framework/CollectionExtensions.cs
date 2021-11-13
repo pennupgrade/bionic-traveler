729f4ca3a0be08e20888ffd93a50e735aa700947
Lennart <lennart@lms-dev.com>
2021-11-13 01:22:58 -0500

    Add background music

---
+++ BionicTraveler/Assets/Framework/CollectionExtensions.cs
@@ -0,0 +1,26 @@
+﻿namespace Framework
+{
+    using System.Collections.Generic;
+
+    /// <summary>
+    /// Contains helper functions for collections.
+    /// </summary>
+    public static class CollectionExtensions
+    {
+        /// <summary>
+        /// Gets a random item from the list.
+        /// </summary>
+        /// <typeparam name="T">The list type.</typeparam>
+        /// <param name="list">The list.</param>
+        /// <returns>The random item.</returns>
+        public static T GetRandomItem<T>(this IList<T> list)
+        {
+            if (list.Count == 0)
+            {
+                throw new System.IndexOutOfRangeException("List is empty.");
+            }
+
+            return list[UnityEngine.Random.Range(0, list.Count)];
+        }
+    }
+}
