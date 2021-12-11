e0873578c20b76e8c3bb98cba4a4633fb55227ed
Chris Yu <chrisyu@Chriss-MacBook-Pro.local>
2021-12-11 18:43:17 -0500

    Add feature ui sfx

--- BionicTraveler/Assets/BionicTraveler/Scripts/Menus/Menu.cs
+++ BionicTraveler/Assets/BionicTraveler/Scripts/Menus/Menu.cs
@@ -4,6 +4,7 @@ namespace BionicTraveler.Scripts
     using System.Collections;
     using System.Collections.Generic;
     using UnityEngine;
+    using BionicTraveler.Scripts.Audio;
 
     /// <summary>
     /// Interface that all menus must implement. Contains an open and close method.
@@ -12,13 +13,19 @@ namespace BionicTraveler.Scripts
     {
         private Canvas canvas; // Canvas containing the menu
 
+        [SerializeField]
+        private AudioClip openSound;
+        [SerializeField]
+        private AudioClip closeSound;
+
         /// <summary>
         /// Start is called before the first frame update.
         /// </summary>
         public virtual void Start()
         {
             this.canvas = this.GetComponent<Canvas>();
-            this.Close();
+            this.canvas.enabled = false;
+            Time.timeScale = 1;
         }
 
         /// <summary>
@@ -26,6 +33,8 @@ namespace BionicTraveler.Scripts
         /// </summary>
         public void Open()
         {
+            
+            AudioManager.Instance.PlayOneShot(openSound);
             this.canvas.enabled = true;
             Time.timeScale = 0;
         }
@@ -35,6 +44,8 @@ namespace BionicTraveler.Scripts
         /// </summary>
         public void Close()
         {
+            
+            AudioManager.Instance.PlayOneShot(closeSound);
             this.canvas.enabled = false;
             Time.timeScale = 1;
         }
