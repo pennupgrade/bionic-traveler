ea8e5dd9307875aa4acefedef49b4658d95da5a6
Lennart <lennart@lms-dev.com>
2022-01-09 13:26:51 -0500

    Stages can now be edited inline within quests

--- BionicTraveler/Assets/Editor/QuestStageEditor.cs
+++ BionicTraveler/Assets/Editor/QuestStageEditor.cs
@@ -5,6 +5,7 @@ namespace BionicTraveler
     using System.Collections;
     using System.Collections.Generic;
     using System.Linq;
+    using System.Reflection;
     using UnityEditor;
     using UnityEngine;
 
@@ -16,14 +17,79 @@ namespace BionicTraveler
     {
         private float extraHeight;
 
+        private static object GetValue_Imp(object source, string name)
+        {
+            if (source == null)
+                return null;
+            var type = source.GetType();
+
+            while (type != null)
+            {
+                var f = type.GetField(name, BindingFlags.NonPublic | BindingFlags.Public | BindingFlags.Instance);
+                if (f != null)
+                    return f.GetValue(source);
+
+                var p = type.GetProperty(name, BindingFlags.NonPublic | BindingFlags.Public | BindingFlags.Instance | BindingFlags.IgnoreCase);
+                if (p != null)
+                    return p.GetValue(source, null);
+
+                type = type.BaseType;
+            }
+            return null;
+        }
+
+        private static object GetValue_Imp(object source, string name, int index)
+        {
+            var enumerable = GetValue_Imp(source, name) as System.Collections.IEnumerable;
+            if (enumerable == null) return null;
+            var enm = enumerable.GetEnumerator();
+            //while (index-- >= 0)
+            //    enm.MoveNext();
+            //return enm.Current;
+
+            for (int i = 0; i <= index; i++)
+            {
+                if (!enm.MoveNext()) return null;
+            }
+            return enm.Current;
+        }
+
+        /// <summary>
+        /// Gets the object the property represents.
+        /// </summary>
+        /// <param name="prop"></param>
+        /// <returns></returns>
+        public static object GetTargetObjectOfProperty(SerializedProperty prop)
+        {
+            if (prop == null) return null;
+
+            var path = prop.propertyPath.Replace(".Array.data[", "[");
+            object obj = prop.serializedObject.targetObject;
+            var elements = path.Split('.');
+            foreach (var element in elements)
+            {
+                if (element.Contains("["))
+                {
+                    var elementName = element.Substring(0, element.IndexOf("["));
+                    var index = System.Convert.ToInt32(element.Substring(element.IndexOf("[")).Replace("[", "").Replace("]", ""));
+                    obj = GetValue_Imp(obj, elementName, index);
+                }
+                else
+                {
+                    obj = GetValue_Imp(obj, element);
+                }
+            }
+            return obj;
+        }
+
         /// <inheritdoc/>
         public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
         {
             var targetObject = property.serializedObject.targetObject;
             var attrib = this.attribute as UnityInheritanceAttribute;
 
-            var inheritanceField = targetObject.GetType().GetField(property.name, System.Reflection.BindingFlags.Instance | System.Reflection.BindingFlags.NonPublic);
-            var inheritance = (UnityInheritance)inheritanceField.GetValue(targetObject);
+            var obj = GetTargetObjectOfProperty(property);
+            var inheritance = (UnityInheritance)obj;
 
             var types = inheritance.DiscoverTypes().ToArray();
 
@@ -50,7 +116,7 @@ namespace BionicTraveler
 
             // Only draw properties that are under our currentProperty.instanceName to exclude all other
             // fields that do not belong to our type.
-            string parentPath = property.name + "." + inheritance.InstanceName + ".";
+            string parentPath = property.propertyPath + "." + inheritance.InstanceName + ".";
             this.extraHeight = popupPosition.height + 2;
             var startPosition = position;
             EditorGUI.indentLevel++;
