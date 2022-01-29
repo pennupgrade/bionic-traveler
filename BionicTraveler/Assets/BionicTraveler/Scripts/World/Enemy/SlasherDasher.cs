12cd29c631a974b6b36ad1132ca3c15b1089d4ce
dluisnothere <dluisnothere@gmail.com>
2022-01-29 18:28:36 -0500

    Add entity task

---
+++ BionicTraveler/Assets/BionicTraveler/Scripts/World/Enemy/SlasherDasher.cs
@@ -0,0 +1,30 @@
+namespace BionicTraveler.Scripts.World.Enemy
+{
+    using System;
+    using System.Collections;
+    using System.Collections.Generic;
+    using UnityEngine;
+
+    /// <summary>
+    /// Please document me.
+    /// </summary>
+    public class SlasherDasher : DynamicEntity
+    {
+        /// <summary>
+        /// Start is called before the first frame update.
+        /// </summary>
+        public void Start()
+        {
+            // push idle
+        }
+
+        /// <summary>
+        /// Update is called once per frame.
+        /// </summary>
+        public void Update()
+        {
+            // If within range of 5 w/ player
+            // push attack action
+        }
+    }
+}
