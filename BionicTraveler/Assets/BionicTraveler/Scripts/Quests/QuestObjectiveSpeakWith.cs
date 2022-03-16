e299b14fa9b59f21cbc98e050cb6dbcc7a0694f6
dluisnothere <dluisnothere@gmail.com>
2022-03-16 01:21:16 -0400

    Add basic quest system Separate map into chunks

---
+++ BionicTraveler/Assets/BionicTraveler/Scripts/Quests/QuestObjectiveSpeakWith.cs
@@ -0,0 +1,54 @@
+﻿namespace BionicTraveler.Scripts.Quests
+{
+    using System;
+    using System.Linq;
+    using BionicTraveler.Scripts.Items;
+    using BionicTraveler.Scripts.World;
+    using UnityEngine;
+
+    /// <summary>
+    /// Quest objective for collecting items. TODO: Make applicable for NPC inventories too?
+    /// </summary>
+    [System.Serializable]
+    public class QuestObjectiveSpeakWith : QuestObjective
+    {
+        [SerializeField]
+        private String targetEntityName;
+
+        /// <inheritdoc/>
+        public override void Activate()
+        {
+            base.Activate();
+        }
+
+        public override object Clone()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        /// <summary>
+        /// Processes a new quest event. Use specific overloads to only process certain events.
+        /// Calls are made from <see cref="QuestStage.ProcessEvent(QuestEvent)"/> through double
+        /// dispatching to select the correct overload.
+        /// </summary>
+        /// <param name="questEventInventory">The event.</param>
+        public void ProcessEvent(QuestEventSpokenTo questEventSpokenTo)
+        {
+            this.CheckTargetName(questEventSpokenTo.personSpokenTo);
+        }
+
+        private void CheckTargetName(String name)
+        {
+            if (name == this.targetEntityName)
+            {
+                Debug.Log("Complete Quest: Spoken to " + name + "!");
+                this.SetAsComplete();
+            }
+            else
+            {
+                Debug.Log("Did not speak with target person");
+                this.SetAsIncomplete();
+            }
+        }
+    }
+}
