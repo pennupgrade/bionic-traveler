d173304de6a7fdd63b9b96f1084773dc22e2d2ba
zedward23 <67485515+zedward23@users.noreply.github.com>
2022-06-04 01:55:49 -0400

    Add Credits Scene

---
+++ BionicTraveler/Assets/BionicTraveler/Scripts/CreditsRoll.cs
@@ -0,0 +1,36 @@
+namespace BionicTraveler.Scripts
+{
+    using System;
+    using System.Collections;
+    using System.Collections.Generic;
+    using UnityEngine;
+
+    /// <summary>
+    /// Please document me.
+    /// </summary>
+    public class CreditsRoll : MonoBehaviour
+    {
+        /// <summary>
+        /// Start is called before the first frame update.
+        /// </summary>
+        public void Start()
+        {
+        }
+
+        /// <summary>
+        /// Update is called once per frame.
+        /// </summary>
+        public void Update()
+        {
+            if (gameObject.transform.position.y < 80)
+            {
+                var pos = gameObject.transform.position;
+                gameObject.transform.position = new Vector3(pos.x, pos.y+.004f, pos.z);
+            } 
+            else
+            {
+                gameObject.transform.position = new Vector3(0, -80, 0);
+            }
+        }
+    }
+}
